6631f349a5357546e52112c56922e31c1b253b28
philblanchard <phil@philblanchard.com>
2022-02-26 16:54:37 -0500

    formatting cleanup

--- DirectLikeTokenFetch.cs
+++ DirectLikeTokenFetch.cs
@@ -4,32 +4,26 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 
-namespace DirectLineTokenFuncProj
+namespace DirectLineTokenFuncProj;
+
+public class DirectLineTokenFetch
 {
-    public class DirectLineTokenFetch
+    private readonly ILogger _logger;
+    private readonly IDlTokenFunction _client;
+    public DirectLineTokenFetch(ILoggerFactory loggerFactory, IDlTokenFunction client)
     {
-        private readonly ILogger _logger;
-        private readonly IDlTokenFunction _client;
-
-        public DirectLineTokenFetch(ILoggerFactory loggerFactory, IDlTokenFunction client)
-        {
-            _logger = loggerFactory.CreateLogger<DirectLineTokenFetch>();
-            _client = client;
-        }
-
-        [Function("DirectLineTokenFetch")]
-        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req)
-        {
-            _logger.LogInformation("Request Processed");
-
+        _logger = loggerFactory.CreateLogger<DirectLineTokenFetch>();
+        _client = client;
+    }
+    [Function("DirectLineTokenFetch")]
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req)
+    {
             
-            var token = await _client.GetToken();
-
-            var response = req.CreateResponse(HttpStatusCode.OK);
-
-            await response.WriteAsJsonAsync(token);
+        _logger.LogInformation("Request Processed");
+        var token = await _client.GetToken();
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        await response.WriteAsJsonAsync(token);
             
-            return response;
-        }
+        return response;
     }
 }
--- DirectLineToken.cs
+++ DirectLineToken.cs
@@ -1,15 +1,12 @@
 using System.Net;
 
 namespace DirectLineTokenFuncProj;
-
 public class DirectLineToken
 {
     public DirectLineToken(Error error)
     {
         Error = error;
     }
-
-
     public string ConversationId { get; set; }
     public string Token { get; set; }
     public int ExpiresIn { get; set; }
@@ -23,7 +20,6 @@ public class Error
         Message = message;
         StatusCode = statusCode;
     }
-
     public string Message { get; set; }
     public HttpStatusCode? StatusCode { get; set; }
 }
